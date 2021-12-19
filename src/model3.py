58c2eb7bc3230fdfcebc09a3973ca52d14229227
Louis Popi <theh2o64@gmail.com>
2021-12-19 11:55:18 +0100

    ai4ia: Change models

--- src/model3.py
+++ src/model3.py
@@ -1,6 +1,6 @@
 # MIT License
 
-# Copyright (c) 2021 Louis Grenioux
+# Copyright (c) 2021 Louis Popi
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
